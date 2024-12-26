3c17916b17da8e487ca2d64d12b0989883a64fb8
NoaBenSimon7 <noabensimon07@gmail.com>
2024-12-26 11:00:03 +0200

    init

---
+++ app/src/main/java/com/example/noaproject/model/Cart.java
@@ -0,0 +1,33 @@
+package com.example.noaproject.model;
+
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Cart  implements Serializable {
+
+    protected ArrayList<ItemOrder> itemOrders;
+
+    public Cart(ArrayList<ItemOrder> itemOrders) {
+        this.itemOrders = itemOrders;
+    }
+
+    public Cart() {
+    }
+
+    public ArrayList<ItemOrder> getItemOrders() {
+        return itemOrders;
+    }
+
+    public void setItemOrders(ArrayList<ItemOrder> itemOrders) {
+        this.itemOrders = itemOrders;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "itemOrders=" + itemOrders +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/noaproject/model/Item.java
@@ -0,0 +1,110 @@
+package com.example.noaproject.model;
+
+import java.io.Serializable;
+
+public class Item  implements Serializable {
+    protected String id, name, type, size, color, fabric, pic;
+    protected double price;
+
+
+
+    public Item(String id, String name, String type, String size, String color, String fabric, String pic, double price) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.size = size;
+        this.color = color;
+        this.fabric = fabric;
+        this.pic = pic;
+        this.price = price;
+    }
+
+    public Item() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFabric() {
+        return fabric;
+    }
+
+    public void setFabric(String fabric) {
+        this.fabric = fabric;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", size='" + size + '\'' +
+                ", color='" + color + '\'' +
+                ", fabric='" + fabric + '\'' +
+                ", pic='" + pic + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
