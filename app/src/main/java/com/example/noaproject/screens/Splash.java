a67013e51fe52e44b694418e6061d17955ac4a68
Student-2024 <student-2024@ashalim.org>
2025-05-23 10:07:06 +0300

    23/05/25

---
+++ app/src/main/java/com/example/noaproject/screens/Splash.java
@@ -0,0 +1,41 @@
+package com.example.noaproject.screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.noaproject.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        Thread mySplashThread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    synchronized (this){
+                        wait(3000);
+                    }
+                }
+                catch(InterruptedException ex){
+                }
+                finish();
+
+                Intent intent = new Intent(Splash.this,Login.class);
+                startActivity(intent);
+            }
+        };
+
+        mySplashThread.start();
+    }
+}
